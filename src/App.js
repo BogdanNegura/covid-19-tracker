abc954461b93c5e6ec200babdd1e83e5fa392619
Bogdan Negura <id.bogdann@yahoo.com>
2020-12-27 21:38:36 +0000

    update the country onChange event and keep it up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FormControl, MenuItem, Select } from '@material-ui/core';
 
 const App= () => {
   const [countries, setCountries] = useState([])
+  const [country, setCountry] =  useState("worldwide")
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -20,12 +21,18 @@ const App= () => {
     getCountriesData()
   },[])
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value
+    setCountry(countryCode)
+  }
+
   return (
     <div className="App">
       <div className="app__header">
         <h1>Covid 19 Tracker</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map(country => <MenuItem value={country.value}>{country.name}</MenuItem>)}
           </Select>
         </FormControl>
